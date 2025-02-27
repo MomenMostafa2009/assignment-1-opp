d42fbaf9f9549c237c7d32e6dd182049f04cd8dc
MomenMostafa2009 <momendarsh2009@gmail.com>
2025-02-27 23:21:01 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace assignment_1_opp
+{
+    enum WeekDays
+    { Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Q1
+            foreach (WeekDays day in Enum.GetValues(typeof(WeekDays)))
+            {
+                Console.WriteLine(day);
+            }
+            #endregion
+
+
+
+        }
+    }
+}
